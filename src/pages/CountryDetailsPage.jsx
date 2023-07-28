5f2de6665f82879a54ad46f08f02537fafa10311
Brian Adams <brianadamsmaino68@gmail.com>
2023-07-28 15:34:49 +0200

    jvj

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,10 @@
-function CountryDetails() {}
+function CountryDetails() {
+  return (
+    <div>
+      <h1>Country Details</h1>
+      {/* Add additional content as needed */}
+    </div>
+  );
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,10 @@
-function HomePage() {}
+function HomePage() {
+  return (
+    <div>
+      <h1>WikiCountries: Your Guide to the World</h1>
+      {/* Add additional content as needed */}
+    </div>
+  );
+}
 
 export default HomePage;
